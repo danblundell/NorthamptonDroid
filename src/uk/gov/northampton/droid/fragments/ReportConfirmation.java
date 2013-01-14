bd250fee5949c77ee9763484d93ece2396e387d2
Dan Blundell <danblundell@me.com>
2013-01-14 14:56:24 +0000

    Done button returns to main activity
    Report confirmation activity now has a 'done' button to return to the
    main activity

--- src/uk/gov/northampton/droid/fragments/ReportConfirmation.java
+++ src/uk/gov/northampton/droid/fragments/ReportConfirmation.java
@@ -2,26 +2,22 @@ package uk.gov.northampton.droid.fragments;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.app.SherlockFragment;
-
 import uk.gov.northampton.droid.Confirmation;
+import uk.gov.northampton.droid.MainActivity;
 import uk.gov.northampton.droid.R;
-import uk.gov.northampton.droid.lib.PostCodeDialogFragment;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ReportConfirmation extends SherlockActivity {
 	
 	private TextView callNumber;
-	private TextView callDescription;
 	private TextView callDueDate;
+	private Button doneButton;
 	
 	
 	@Override
@@ -42,11 +38,24 @@ public class ReportConfirmation extends SherlockActivity {
 			callNumber.setText(conf.getCallNumber());
 			callDueDate.setText(conf.getSla());
 		}
+		doneButton.setOnClickListener(doneButtonListener);
 	}
 	
 	private void findAllViewsById(){
 		callNumber = (TextView) this.findViewById(R.id.conf_case_ref_TextView);
-		callDescription = (TextView) this.findViewById(R.id.conf_description_TextView);
-		callDueDate = (TextView) this.findViewById(R.id.conf_description_bottom_TextView);
+		callDueDate = (TextView) this.findViewById(R.id.conf_due_date_TextView);
+		doneButton = (Button) this.findViewById(R.id.reportDoneButton);
 	}
+	
+	private OnClickListener doneButtonListener = new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+			startActivity(intent);
+		}
+		
+	};
 }
